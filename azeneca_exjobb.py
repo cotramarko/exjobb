8063a556c7cb05c836b6773b6a4bf32f744dddaa
fredrikjacobson <fredrik.jacobson93@gmail.com>
2016-02-23 22:46:27 +0100

    Astra Zeneca exjobb added

---
+++ azeneca_exjobb.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import os
+
+
+chromedriver = "/Users/fredrikjacobson/Desktop/code/chromedriver" #change to local chromedriver location
+os.environ["webdriver.chrome.driver"] = chromedriver
+browser = webdriver.Chrome(chromedriver)
+
+browser.get("http://jobs.astrazeneca.com")
+
+
+#element = browser.find_element_by_xpath("""//*[@id="top"]/div/div[2]/div[5]/div[4]/p[2]/a/strong""")
+#browser.execute_script("return arguments[0].scrollIntoView();", element)
+
+
+browser.find_element_by_id("316").click() #sweden
+time.sleep(1)
+browser.find_element_by_id("1257").click() #student opportunities
+time.sleep(1)
+browser.find_element_by_class_name('submit').click()
+
+job_list = browser.find_elements_by_class_name('job-res-description')
+
+for l in job_list:
+    
+    title = l.find_element_by_class_name('job-title')
+    if 'diploma' in title.text.lower():
+        location = l.find_element_by_class_name('locations')
+        print('Title: {}\nLocation: {}\nLink: {}\n\n'.format(title.text, location.text, title.get_attribute('href')))
+
+
+

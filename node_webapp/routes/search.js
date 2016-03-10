7075d9a8bc2a65a4680739fb552c05281a256fa8
fredrikjacobson <fredrik.jacobson93@gmail.com>
2016-03-11 00:03:30 +0100

    deletion of node_webapp

--- node_webapp/routes/search.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/searching', function(req, res){
- res.send("WHEEE");
-});
-module.exports = router;
-

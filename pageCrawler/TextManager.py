8dfe01ececd21e869b5d44dd8cfe267a18e006f0
Marko Cotra <cotra.marko@gmail.com>
2016-04-16 16:49:02 +0200

    removed empty files and a symbolik link, added a TextManager class that will be in charge of text elements in CrawlerData objects

---
+++ pageCrawler/TextManager.py
@@ -0,0 +1,6 @@
+'''
+Is in charge of managing all text elements of a CrawlerData object, such as: 
+	1. Making sure that each thesis title contains a certain keyword. 
+	2. Renaming the spelling of cities and regions. 
+'''
+
